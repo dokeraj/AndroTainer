af9cceadb8dee388efed905d75b8b0ddcbf4976f
Felix Fischer <fischerfelix06@gmail.com>
2023-01-03 18:42:29 +0100

    https://github.com/google/dagger/releases/tag/dagger-2.34

--- app/src/main/java/com/dokeraj/androtainer/viewmodels/DockerListerViewModel.kt
+++ app/src/main/java/com/dokeraj/androtainer/viewmodels/DockerListerViewModel.kt
@@ -1,22 +1,23 @@
 package com.dokeraj.androtainer.viewmodels
 
-import androidx.hilt.Assisted
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.dokeraj.androtainer.models.ContainerActionType
 import com.dokeraj.androtainer.models.ContainerStateType
 import com.dokeraj.androtainer.models.Kontainer
 import com.dokeraj.androtainer.repositories.DockerListerRepo
 import com.dokeraj.androtainer.util.DataState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
 class DockerListerViewModel
-@ViewModelInject constructor(
+@Inject constructor(
     private val dockerListerRepo: DockerListerRepo,
-    @Assisted private val savedStateHandle: SavedStateHandle,
+    private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
     private val _dataState: MutableLiveData<DataState<List<Kontainer>>> = MutableLiveData()
